55b2ff08f7b09803eb17aab56382d1de4f6de5c4
C1310171 Andre Hernandez Bargas <ahbargas@latam.stefanini.com>
2019-11-25 16:42:54 -0300

    Criacao de MENU

--- README.md
+++ README.md
@@ -8,7 +8,23 @@ O Objetivo deste projeto é a construção de um sistema baseado no paradigmas d
 
 ## Como executar
 
-Para executar o projeto basta executar no terminal :
+Para executar o projeto é necessario a instalacao e configuracao de um servidor XMPP. 
+É recomendada a instalaçao do Prosody IM, sendo setado para que o servidor seja levantado no 
+"localhost". 
 
+Como instalar o Prosody IM : https://prosody.im/
+
+Outras opções de servidores XMPP : https://xmpp.org/software/servers.html     
+
+
+Após configurado o servidor basta a instalação da bibliteca Spade
+
+```console
 sudo pip3 install spade
+```
+
+E finalmente a execussão do programa:
+
+```console
 python3 main.py
+```
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import time
+from asyncio.base_futures import CancelledError
+import os
 from spade import quit_spade
 from spade.agent import Agent
 from spade.behaviour import OneShotBehaviour
@@ -40,25 +42,64 @@ def create_player(player_number, judge_jid):
     return player
 
 
+def create_user_player(jogada, judge_jid):
+    player_jid = "p_{}@localhost".format("user")
+    player_passwd = "secret"
+
+    player = UserPlayerAgent(jogada, judge_jid, player_jid, player_passwd)
+    future = player.start(auto_register=True)
+    future.result()
+    print("UserPlayer iniciado!")
+
+    return player
+
+
+class UserPlayerAgent(Agent):
+
+    # Inner class para a definicao do comportamento.
+    class PlayBehav(OneShotBehaviour):
+        async def run(self):
+            # print("PlayBehav esta sendo executado!")
+            msg = Message(to=self.agent.recv_jid)
+            msg.set_metadata("performative", "inform")  # Set the "inform" FIPA performative
+
+            msg.body = "user-" + self.agent.jogada  # Set the message content
+
+            await self.send(msg)
+            # print("Jogada foi mandada!")
+
+            # stop agent from behaviour
+            await self.agent.stop()
+
+    async def setup(self):
+        print("UserPlayerAgent foi iniciado")
+        b = self.PlayBehav()
+        self.add_behaviour(b)
+
+    def __init__(self, jogada, recv_jid, *args, **kwargs):
+        self.recv_jid = recv_jid
+        self.jogada = jogada
+        super().__init__(*args, **kwargs)
+
+
 class PlayerAgent(Agent):
 
     # Inner class para a definicao do comportamento.
     class PlayBehav(OneShotBehaviour):
         async def run(self):
-            print("PlayBehav esta sendo executado!")
             msg = Message(to=self.agent.recv_jid)
             msg.set_metadata("performative", "inform")  # Set the "inform" FIPA performative
 
             msg.body = self.agent.player_n + "-" + choice(self.agent.items)  # Set the message content
 
             await self.send(msg)
-            print("Jogada foi mandada!")
+            # print("Jogada foi mandada!")
 
             # stop agent from behaviour
             await self.agent.stop()
 
     async def setup(self):
-        print("PlayerAgent, number:", self.player_n, " foi iniciado")
+        # print("PlayerAgent foi iniciado")
         b = self.PlayBehav()
         self.add_behaviour(b)
 
@@ -72,7 +113,6 @@ class PlayerAgent(Agent):
 class JudgeAgent(Agent):
     class RecvBehav(OneShotBehaviour):
         async def run(self):
-            print("RecvBehav running")
 
             # Recebendo menagem do primeiro jogador
             msg = await self.receive(timeout=10)  # wait for a message for 10 seconds
@@ -86,7 +126,7 @@ class JudgeAgent(Agent):
             msg = await self.receive(timeout=10)
             if msg:
                 jogada_2 = msg.body.split("-")[1]
-                print("Player 1 jogou : {}".format(jogada_2))
+                print("Player 2 jogou : {}".format(jogada_2))
             else:
                 print("Did not received any message after 10 seconds")
             # stop agent from behaviour
@@ -98,33 +138,133 @@ class JudgeAgent(Agent):
             await self.agent.stop()
 
     async def setup(self):
-        print("ReceiverAgent started")
         b = self.RecvBehav()
         template = Template()
         template.set_metadata("performative", "inform")
         self.add_behaviour(b, template)
 
 
-if __name__ == "__main__":
+def show_menu():
+
+    print("BEM VINDO AO MULTI JOKEN-PO")
+    print("Selecione uma da opcoes ")
+    print("1 - Jogar com um agente autonomo ")
+    print("2 - Agentes autonomos jogando entre si")
+    print("3 - Sair ")
+
+
+def getJogada():
+    while True:git 
+        jogada = input("Qual a jogada voce quer fazer ? (pedra, papel ou tesoura) \n")
+
+        if jogada in ["pedra", "papel", "tesoura"]:
+            return jogada
+        else:
+            continue
+
+def do_user_play():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    judge_jid = "j@localhost"
+    judge_passwd = "secret"
+    judge_agent = JudgeAgent(judge_jid, judge_passwd)
+    future = judge_agent.start(auto_register=True)
+    future.result()
+
+
+    print("Agentes jogando contra usuario : \n")
+
+    jogada = getJogada()
+    user = create_user_player(jogada, judge_jid)
+
+    player1 = create_player("2", judge_jid)
+
+    exit()
+    while judge_agent.is_alive():
+        try:
+            time.sleep(2)
+            player1.stop()
+            user.stop()
+            judge_agent.stop()
+            exit()
+            break
+        except KeyboardInterrupt:
+            player1.stop()
+            user.stop()
+            judge_agent.stop()
+            break
+    quit_spade()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def do_agents_play():
+
+    os.system('cls' if os.name == 'nt' else 'clear')
 
     judge_jid = "j@localhost"
     judge_passwd = "secret"
     judge_agent = JudgeAgent(judge_jid, judge_passwd)
     future = judge_agent.start(auto_register=True)
     future.result()
-    print("Receiver started")
+
+
+
+    print("Agentes jogando entre si : \n")
 
     player1 = create_player("1", judge_jid)
     player2 = create_player("2", judge_jid)
 
+
+
+
+    exit()
     while judge_agent.is_alive():
         try:
-            time.sleep(1)
-            print(".", end="")
+            time.sleep(2)
+            player1.stop()
+            player2.stop()
+            judge_agent.stop()
+            exit()
+            break
         except KeyboardInterrupt:
             player1.stop()
             player2.stop()
             judge_agent.stop()
             break
-    print("Agents finished")
     quit_spade()
+
+
+if __name__ == "__main__":
+
+    show_menu()
+    opcao = input()
+
+    while opcao != "3":
+        if opcao == "1":
+            do_user_play()
+
+        elif opcao == "2":
+
+            do_agents_play()
+        else:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print("Opcao invalida !!!\n")
+            show_menu()
+            opcao = input()
+
+
